d5f44982d8a63f4f17ed166d966aaed9de00e93c
user11 <pehotskijdanil@gmail.com>
2022-08-21 13:28:17 +0200

    homework

---
+++ src/homework.java
@@ -0,0 +1,23 @@
+public class homework{
+    public static void main(String[] args) {
+        int[]monsterenergy = new int[] {10,20,30,40,50,60};
+        printElements(monsterenergy);
+
+    }
+
+    public static void printElements(int[] ints){
+        for(int a=0;a<10;a++) {
+            if (a == 5) {
+                break;
+            }
+            System.out.println(a);
+        }
+
+
+
+    }
+
+
+
+    }
+
